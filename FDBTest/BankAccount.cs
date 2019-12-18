b90050780972fdf59fd51f502619b7231a415091
David Austin <DavidWayneAustin@outlook.com>
2019-12-18 15:17:33 +0000

    Adding class

---
+++ FDBTest/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FDBTest
+{
+    public class BankAccount
+    {
+        string _customerName;
+        double _balance;
+
+        public BankAccount(string customerName, double balance)
+        {
+            _customerName = customerName;
+            _balance = balance;
+        }
+
+        public double Balance { get { return _blance; } }
+    }
+}
+
